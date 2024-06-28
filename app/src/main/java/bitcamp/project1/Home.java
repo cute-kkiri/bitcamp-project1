dc294ac9dbc4d89fe5ff334bc6ddfee803621eef
SA030 <suna_0132@naver.com>
2024-06-28 09:46:26 +0900

    comment

--- app/src/main/java/bitcamp/project1/Home.java
+++ app/src/main/java/bitcamp/project1/Home.java
@@ -1,15 +1,8 @@
 package bitcamp.project1;
 
-import bitcamp.project1.Util.AccountCommand;
 import bitcamp.project1.Util.MenuCommand;
-import bitcamp.project1.Util.UserCommand;
 import bitcamp.project1.page.Menu;
-import bitcamp.project1.vo.Account;
-import bitcamp.project1.vo.User;
 
-import java.util.LinkedList;
-
-public class Home extends MenuCommand {
 //  [page]
 //    |
 //    | (*Not implemented)
@@ -33,18 +26,9 @@ public class Home extends MenuCommand {
 // -ID                                         -description
 // -PW
 
-
-
-
-
+public class Home extends MenuCommand {
   public static void main(String[] args) {
-
-
-
     Menu m = new Menu();
     m.menu();
-    
   }
-
-
 }// Class Home END
